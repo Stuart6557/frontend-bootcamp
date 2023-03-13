4ab455033ede34ec5e7a2f394a822fde11836943
Stuart6557 <vivianliu532@gmail.com>
2023-03-13 01:14:13 -0700

    messed around with routes and pages

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import "./styles.css";
 import Counter from "./Counter";
 import BlogPosts from "./BlogPosts";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Counter</h1>
-      <BlogPosts />
-      <Counter counterLabel="Number" />
-      <Counter counterLabel="Count" />
-      <Counter counterLabel="Age" />
+      <h1>Modified Frontend Bootcamp</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ [<a href="/blog"><button class="path-button">Click here to display blog posts</button></a>,
+              <h1>Counter</h1>,
+              <Counter counterLabel="Number" />,
+              <Counter counterLabel="Count" />,
+              <Counter counterLabel="Age" />] } />
+          <Route path="/blog" element={[<a href="/"><button class="path-button">Click here to display counts</button></a>,
+              <h1>Blog Posts</h1>,
+              <BlogPosts />]} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
